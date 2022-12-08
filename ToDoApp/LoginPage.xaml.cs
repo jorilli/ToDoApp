d7db1b5698f910d2ae74f4ab8737d6770f2cb846
sPouletMeitli <105846481+sPouletMeitli@users.noreply.github.com>
2022-12-08 15:15:24 +0100

    initial commit

---
+++ ToDoApp/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ToDoApp;
+
+public partial class LoginPage : ContentView
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ToDoApp/NewContent1.xaml.cs
@@ -0,0 +1,9 @@
+namespace ToDoApp;
+
+public partial class NewContent1 : ContentView
+{
+	public NewContent1()
+	{
+		InitializeComponent();
+	}
+}
